48535639907adaf326f3bece664f96c80d6e9173
Kovaleva Marina <kirienko1995@mail.ru>
2024-08-28 12:58:20 +0300

    сверстать navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Highlights from "./components/Highlights.jsx";
 
 const App = () => {
 
   return (
-      <h1 className="text-3xl font-bold underline">
-          Hello world!
-      </h1>
+      <main className="bg-black">
+          <Navbar/>
+          <Hero/>
+          <Highlights/>
+      </main>
   )
 }
 
