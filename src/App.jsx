38e203a820d59f367e1a0cd6a81bd35b1188d467
Kovaleva Marina <kirienko1995@mail.ru>
2024-10-28 16:57:12 +0300

    feat: добавить секцию Features

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Highlights from "./components/Highlights.jsx";
 import Model from "./components/Model.jsx";
+import Features from "./components/Features.jsx";
 
 const App = () => {
 
@@ -11,6 +12,7 @@ const App = () => {
           <Hero/>
           <Highlights/>
           <Model/>
+          <Features/>
       </main>
   )
 }
