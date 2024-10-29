adc04c0033348176f5a53b38e3f20418dcbb5c2f
Kovaleva Marina <kirienko1995@mail.ru>
2024-10-29 10:32:19 +0300

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -66,8 +66,8 @@ const Features = () => {
                                     <p className="feature-text g_text">
                                         iPhone 15 Pro is {' '}
                                         <span className="text-white">
-                      the first iPhone to feature an aerospace-grade titanium design
-                    </span>,
+                                            the first iPhone to feature an aerospace-grade titanium design
+                                        </span>,
                                         using the same alloy that spacecrafts use for missions to Mars.
                                     </p>
                                 </div>
@@ -76,13 +76,11 @@ const Features = () => {
                                     <p className="feature-text g_text">
                                         Titanium has one of the best strength-to-weight ratios of any metal, making these our {' '}
                                         <span className="text-white">
-                      lightest Pro models ever.
-                    </span>
+                                            lightest Pro models ever.
+                                        </span>
                                         You'll notice the difference the moment you pick one up.
                                     </p>
                                 </div>
-
-
                             </div>
                         </div>
                     </div>
